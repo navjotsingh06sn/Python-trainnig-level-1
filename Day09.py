8b15e7f2442a968b8579ad512aab4a740ee5c8a5
navjotsingh06sn <clearnavjotsingh06sn@gmail.com>
2026-07-20 15:07:38 +0530

    MONGODB

---
+++ Day09.py
@@ -0,0 +1,33 @@
+# pre requisite - mtlb jo chij pehla annii chaihidi aa kujj v krn toh 
+# MONGODB 
+# default port = 27017
+#1. stores non relatonal data
+# 2. stores data in key-values pair..
+# 3. stores data locally and on cloud also
+# 4. record in mongodb are called documents and it can store nested documents also....
+# to store data locally we use COMPASS and to store data on cloud we use ATLAS...
+
+# step 1
+#  open cluster in mongodb atlas and press on connect
+
+# step 2 
+# select compass and copy that link
+# mongo=mongodb+srv://Navjotaug30:navjot%403006@cluster0.odqraq6.mongodb.net/Rbu-managemenent?retryWrites=true&w=majority
+
+# step3
+# create a env file in vs studio and paste the mongodb url
+
+# step 4 add schema in the end (database)
+# can be anything
+
+# step 5 
+# write a security (?retryWrites=true&w=majority)
+
+# step 6
+# if use @ in password we replace it with %40
+
+# step 7 
+# if <db_password>is shown replace this with ur password
+
+# step 8 
+# install the compass and paste the mongo db url 
