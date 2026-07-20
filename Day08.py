3c38f12acafb007840fdc3248f57a50df40eaae7
navjotsingh06sn <clearnavjotsingh06sn@gmail.com>
2026-07-20 15:07:09 +0530

    Mysql

---
+++ Day08.py
@@ -0,0 +1,47 @@
+# MY Sql
+Mysqldb="mysql://root:Admission100439@localhost:3306/sys"
+# To create data base :- create database rbu;
+# To show database;
+# To deleted data base :- drop database rbu;
+
+# Used to create table 
+
+# use rbu;
+# create table students(
+# id int (50),
+# name varchar(20),
+# class varchar(30),
+# age int
+# );
+
+# To show need to open new sheet SQL file name 1 :-  select * from stundents;
+# To show after reopen Mysql :- select * from rbu.stuudents;
+
+# To insert multiple
+# insert into students
+# values(1,'Satnam','BTech','20'),
+# (2,'Armann','BTech','20')
+
+# Update
+# update students
+# set age=20,
+#  name='Navjot',
+#  class='Btech',
+#  id=2
+# where id=34;
+
+# Delete 
+# Delete from students
+# where id=4;
+
+#Fetch
+# Select a particular row
+# select * from rbu.students
+# where id=2;
+# Select a particular column
+# Select name from rbu.students
+# To select multiple data from large data set
+# select * from rbu.students
+# limit 2;
+
+
