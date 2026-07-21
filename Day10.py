fc27e3b7fc5d30d7de2ce28e25990140c5954ff7
navjotsingh06sn <clearnavjotsingh06sn@gmail.com>
2026-07-21 10:41:04 +0530

    AI

---
+++ Day10.py
@@ -0,0 +1,30 @@
+# How AI models work
+# AI is a ability of machine to perform task that required human intelligence.
+# Such as ,learning ,reasoning ,decision-making ,problem-solving.....eg:- chatGPT ,Siri , Self Driving cars ,Face recognization etc.
+
+# AI Milestones:
+# 1950- Alan Turning prossees the turning test.
+# 1956- AI was coied at Dartmouth Wrokshop.
+# 1966- ELIZA (first chatbot) is developed.
+# 1997- Deep Blue defeats world chesss champion Garry Kasparov.
+# 2011- IBM Waston win the quiz show Jeopardy!
+# 2016- AlphaGo defeat Lee Sedol in Go.
+# 2022- ChatGPT is released for public use, popularized gernative AI
+
+# AI Tools:- ChatGPT ,Google Gemini ,Claude ,Siri ,Perplexity ,Grock 
+# AI Development-Tools
+# windsurf:- Next gen AI ide(Integerated Development Environment)
+# Cursor:- AI Powered code editor build on VScode.
+# Anti Gravity ,Kiro:- It is a tool/IDE help to build software.
+
+# LLM(Large Language Model):- It is type of AI that trained massive amount of text to understand or that can read ,Write , answer question
+# ,translate language and summarized text and generate code.
+
+# Type of AI :-
+# Narrow AI(Weak AI):- Perform specific task. Eg- ChatGPT, Siri ,Google Map.
+# Gerneral AI(AGI(Artificial General Intelligence)):- Strong AI Can perform any intellectual task like human Eg- Does not exist yet.
+# Super AI:- More inteliigent then human in all aspects/area Eg:- Does not exist.
+
+# AI Model:- It is atrained program that learn from data and perform task such as answering question ,recoganize image oor generate text.
+
+
