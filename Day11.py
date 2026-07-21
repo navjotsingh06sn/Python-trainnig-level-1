646ca8990842f9aee8918a38d82f0b732f410c90
navjotsingh06sn <clearnavjotsingh06sn@gmail.com>
2026-07-21 10:42:11 +0530

    Node.js

---
+++ Day11.py
@@ -0,0 +1,37 @@
+# Download Node.js
+# 0.0.0.0/0 = tis access list entry potentially allows access to all IPv4 addresses(limited device connected)
+
+# Backend 
+# server.js or index.js
+# entry point of the application 
+# start the server
+# connect mildware and routes(mildware = checkpoint before the root)
+
+# DB connection 
+# connect your app to the database
+
+# Model
+# define the structure of data in database
+
+# Controller
+# 1. contain bussiness logic
+# 2. recive request and send response
+
+# routers
+# define url and map them to controller
+
+# JWT =Jason web token 
+# is used to verify the user identity
+
+# port - 
+# port computer da ik logical gate hunda hai jithe network traffic enter ya exit krda hai
+
+# cors - cross origin resourse sharing 
+# CORS ik security rule hai jo decide karda hai ki ik website duji website / server ton data access kr sakdi ya nhi
+
+# environment variable - an environment variable is a named value that stores configuration setting and can be accessed by an application while it is runnig
+# .env - it is a file used to store environment variable for an application in one place
+# environment variable is stored in .env file
+
+# total schema = total router = total controller..... jitne schemas hote hai utne hi routers honge and utne hi controller honge
+
