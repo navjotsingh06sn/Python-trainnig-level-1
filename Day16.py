c09b7bc2cd396debf098f970abf7f8b0e378f8c8
navjotsingh06sn <clearnavjotsingh06sn@gmail.com>
2026-07-24 10:33:52 +0530

    portfolio

---
+++ Day16.py
@@ -0,0 +1,5 @@
+# overleaf best platform for resume - letxcode 
+# figma stitch best platform for designing resume
+# biased means fully inspired honna
+# portfolio:- portfolio is a collection of work samples and documents that showcase your skills, experience, and achievements.
+# It is often used by professionals to demonstrate their capabilities to potential employers or clients.
